129333201ad01158c250b3c20c3ee52e7dc7f23c
kehvarl <kehvarl@kehvarl.com>
2025-11-30 22:26:10 -0700

    Day 01  04:14/18:01
    Reminder:  Stop and breathe.  It's not actually a competition!  Also, read your code before you change it.

--- day01/pt1.py
+++ day01/pt1.py
@@ -4,7 +4,19 @@ from pprint import pprint
 with open("input.txt") as f:
     content = [x.strip() for x in f.readlines()]
     # content = [int(x) for x in f.readlines()]
+dial = 50
+zeros = 0
 
-for y, v1 in enumerate(content):
-    for x, v2 in enumerate(v1):
-        pass
+for line in content:
+    d = line[0]
+    count = int(line[1:])
+    if d == "L":
+        dial =  (dial - count) % 100
+    else:
+        dial =  (dial + count) % 100
+
+    print(line, dial)
+    if dial == 0:
+        zeros += 1
+
+print(zeros)
--- day01/pt2.py
+++ day01/pt2.py
@@ -1,12 +1,29 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 from pprint import pprint
 
 with open("input.txt") as f:
-    content = f.readlines()
-    #content = [int(x) for x in f.readlines()]
+    content = [x.strip() for x in f.readlines()]
+    # content = [int(x) for x in f.readlines()]
+dial = 50
+zeros = 0
+
+for line in content:
+    d = line[0]
+    count = int(line[1:])
+    if d == "L":
+        for i in range(count, 0, -1):
+            dial -= 1
+            dial %= 100
+            if dial == 0:
+                zeros += 1
+    else:
+        for i in range(count, 0, -1):
+            dial += 1
+            dial %= 100
+            if dial == 0:
+                zeros += 1
+
+    print(line, dial, zeros)
 
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+print(zeros)
