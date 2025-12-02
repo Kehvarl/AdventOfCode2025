05e4a99c5a64c924193a19725385c03489b74cd1
kehvarl <kehvarl@kehvarl.com>
2025-12-01 22:26:37 -0700

    Day 02 09:59 / 17:26
    That one really messed with my head at first.  And then my worst fears were validated for part 2... fortunately it wasn't as bad as I was thinking it would be

--- day02/pt1.py
+++ day02/pt1.py
@@ -2,9 +2,21 @@ from collections import defaultdict, deque
 from pprint import pprint
 
 with open("input.txt") as f:
-    content = [x.strip() for x in f.readlines()]
+    content = f.readline().strip()
     # content = [int(x) for x in f.readlines()]
 
-for y, v1 in enumerate(content):
-    for x, v2 in enumerate(v1):
-        pass
+content = content.split(",")
+total = 0
+for r in content:
+    start, end = r.split("-")
+    start = int(start)
+    end = int(end)
+    for val in range(start, end+1):
+        s = str(val)
+        l = s[:len(s)//2]
+        r = s[len(s)//2:]
+        if l == r:
+            print(s, l, r)
+            total += val
+
+print(total)
--- day02/pt2.py
+++ day02/pt2.py
@@ -1,12 +1,30 @@
-from collections import defaultdict
+from collections import defaultdict, deque
+from operator import truediv
 from pprint import pprint
 
 with open("input.txt") as f:
-    content = f.readlines()
-    #content = [int(x) for x in f.readlines()]
+    content = f.readline().strip()
+    # content = [int(x) for x in f.readlines()]
 
+def get_substrings(s):
+    for x in range(1,len(s)):
+        sub = s[:x]
+        if sub * (len(s) // len(sub)) == s:
+            return True
+    return False
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+content = content.split(",")
+total = 0
+for r in content:
+    start, end = r.split("-")
+    start = int(start)
+    end = int(end)
+    for val in range(start, end+1):
+        s = str(val)
+        l = s[:len(s)//2]
+        r = s[len(s)//2:]
+        if get_substrings(s):
+            #print(s, l, r)
+            total += val
+
+print(total)
