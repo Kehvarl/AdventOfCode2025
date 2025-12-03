7f1a180b82e112d53eb36c0e3d6b69a85a597a15
kehvarl <kehvarl@kehvarl.com>
2025-12-02 22:26:42 -0700

    Day 03 - 12:19 / 24:08

--- day03/pt1.py
+++ day03/pt1.py
@@ -5,6 +5,25 @@ with open("input.txt") as f:
     content = [x.strip() for x in f.readlines()]
     # content = [int(x) for x in f.readlines()]
 
-for y, v1 in enumerate(content):
-    for x, v2 in enumerate(v1):
-        pass
+banks = []
+total = 0
+for line in content:
+    best = 0
+    i = 0
+    for c in range(len(line)-1):
+        v = int(line[c])
+        if v > best:
+            best = v
+            i = c
+    b2 = 0
+    i2 = 0
+    for c in range(i+1, len(line)):
+        v = int(line[c])
+        if v > b2:
+            b2 = v
+            i2 = c
+    #print(i, best, i2, b2)
+    #print(best, b2)
+    total += best*10
+    total += b2
+print(total)
--- day03/pt2.py
+++ day03/pt2.py
@@ -1,12 +1,28 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 from pprint import pprint
 
 with open("input.txt") as f:
-    content = f.readlines()
-    #content = [int(x) for x in f.readlines()]
+    content = [x.strip() for x in f.readlines()]
+    # content = [int(x) for x in f.readlines()]
 
+def scan(bank, start, count):
+    best = 0
+    i= 0
+    for c in range(start, len(bank)-(count-1)):
+        v = int(bank[c])
+        if v > best:
+            best = v
+            i = c
+    return i, best
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+banks = []
+total = 0
+for line in content:
+    batteries = 12
+    i = -1
+    for c in range(batteries, 0, -1):
+        i, b = scan(line, i+1, c)
+        #print(i, b, c, b*(10**(c-1)))
+        total += b*(10**(c-1))
+
+print(total)
