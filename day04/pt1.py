ae1a6b7208a5652b09b9023b0b4c58241691e5bd
kehvarl <kehvarl@kehvarl.com>
2025-12-03 22:30:16 -0700

    Day 04 - 16:59 / 24:12
    This time I read the problem properly, but I didn't read my code properly.

--- day04/pt1.py
+++ day04/pt1.py
@@ -5,6 +5,30 @@ with open("input.txt") as f:
     content = [x.strip() for x in f.readlines()]
     # content = [int(x) for x in f.readlines()]
 
+neighbors = [(-1,-1), (-1,0), (-1,1),(0,-1), (0,1),(1,-1), (1,0), (1,1)]
+
+safe = 0
 for y, v1 in enumerate(content):
+    line = ""
     for x, v2 in enumerate(v1):
-        pass
+        if v2 == ".":
+            line += v2
+            continue
+        rolls = 0
+        for n in neighbors:
+            dx,dy = n
+            tx = x + dx
+            ty = y + dy
+            if 0 <= tx < len(v1) and 0 <= ty < len(content):
+                if content[ty][tx] == "@":
+                   # print(y, x, dx, dy,  ty, tx, content[ty][tx])
+                    rolls += 1
+        if rolls <4:
+            safe += 1
+            line += "X"
+        else:
+            line += str(rolls)
+    print(line)
+
+print(safe)
+
--- day04/pt2.py
+++ day04/pt2.py
@@ -1,12 +1,46 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 from pprint import pprint
 
 with open("input.txt") as f:
-    content = f.readlines()
-    #content = [int(x) for x in f.readlines()]
+    content = [x.strip() for x in f.readlines()]
+    # content = [int(x) for x in f.readlines()]
+
+neighbors = [(-1,-1), (-1,0), (-1,1),(0,-1), (0,1),(1,-1), (1,0), (1,1)]
+
+def step(content):
+    removed = 0
+    out = []
+    for y, v1 in enumerate(content):
+        line = ""
+        for x, v2 in enumerate(v1):
+            if v2 == ".":
+                line += v2
+                continue
+            rolls = 0
+            for n in neighbors:
+                dx,dy = n
+                tx = x + dx
+                ty = y + dy
+                if 0 <= tx < len(v1) and 0 <= ty < len(content):
+                    if content[ty][tx] == "@":
+                        rolls += 1
+            if rolls <4:
+                removed += 1
+                line += "."
+            else:
+                line += v2
+        out.append(line)
+    return out, removed
 
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+out, removed = step(content)
+steps = 1
+total = 0
+while removed > 0:
+    #pprint(removed)
+    steps += 1
+    total += removed
+    out, removed = step(out)
+
+pprint(out)
+print(steps, total)
