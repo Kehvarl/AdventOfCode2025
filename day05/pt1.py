7a734be3160a90839edb825f2c42ea28e2506fca
Adam <ACorbett@SecureSourceUSA.com>
2025-12-04 22:35:29 -0700

    Day 05: 16:45 / 28:14

--- day05/pt1.py
+++ day05/pt1.py
@@ -2,9 +2,26 @@ from collections import defaultdict, deque
 from pprint import pprint
 
 with open("input.txt") as f:
-    content = [x.strip() for x in f.readlines()]
+    raw = f.read()
+    #content = [x.strip() for x in f.readlines()]
     # content = [int(x) for x in f.readlines()]
 
-for y, v1 in enumerate(content):
-    for x, v2 in enumerate(v1):
-        pass
+range_fresh, have = raw.split("\n\n")
+
+fresh = []
+for line in range_fresh.split("\n"):
+    start, end = line.split("-")
+    fresh.append([int(start), int(end) + 1])
+
+good = set()
+bad = set()
+for h in have.strip().split("\n"):
+    test = int(h)
+    for f in fresh:
+        if f[0] <= test <= f[1]:
+            good.add(int(h))
+        else:
+            bad.add(int(h))
+
+print(len(good), good)
+print(len(bad), bad)
--- day05/pt2.py
+++ day05/pt2.py
@@ -1,12 +1,28 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 from pprint import pprint
 
 with open("input.txt") as f:
-    content = f.readlines()
-    #content = [int(x) for x in f.readlines()]
+    raw = f.read()
+    #content = [x.strip() for x in f.readlines()]
+    # content = [int(x) for x in f.readlines()]
 
+range_fresh, have = raw.split("\n\n")
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+fresh = []
+for line in range_fresh.split("\n"):
+    start, end = line.split("-")
+    fresh.append((int(start), int(end)))
+
+fresh.sort()
+total = 0
+high = 0
+for s,e in fresh:
+    print(high, s, e)
+    if high < s:
+        total += (e - s + 1)
+        high = e + 1
+    elif high <= e:
+        total += (e - high + 1)
+        high = e + 1
+
+print(total)
