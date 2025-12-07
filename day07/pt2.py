f55471efbb2d8de422d592e23ae30457dec2abde
kehvarl <kehvarl@kehvarl.com>
2025-12-06 23:10:20 -0700

    Day 07 - 22:38 / 1:01:05
    Well that took a while.  Some solvers are saying it was "easy" and for a Saturday maybe that's true, but I don't think I could have done it a couple years ago.

--- day07/pt2.py
+++ day07/pt2.py
@@ -1,12 +1,33 @@
-from collections import defaultdict
+from collections import defaultdict, deque, Counter
 from pprint import pprint
 
 with open("input.txt") as f:
-    content = f.readlines()
-    #content = [int(x) for x in f.readlines()]
+    content = [x.strip() for x in f.readlines()]
+    # content = [int(x) for x in f.readlines()]
 
+beams = [] #set()
+splitters = defaultdict(int)
+splits = 0
+start = None
+for x, v2 in enumerate(content[0]):
+    if v2 == "S":
+        start = x
+        break
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+quantum = Counter({start: 1})
+
+for y, v1 in enumerate(content[1:]):
+    new_quantum = Counter()
+    for x in quantum:
+        c = quantum[x]
+        if v1[x] == "^":
+            new_quantum[x-1] += c
+            new_quantum[x+1] += c
+        else:
+            new_quantum[x] += c
+    quantum = new_quantum
+
+sum = 0
+for x in quantum:
+    sum += quantum[x]
+print(sum)
