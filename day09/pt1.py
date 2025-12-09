c9eeac63f4214767446c8cfbfd81523f5ed4ef27
kehvarl <kehvarl@kehvarl.com>
2025-12-08 22:59:02 -0700

    Day 09 - 10:29 / 55:37
    Wow, I'm not good at these geometry things.  I'm certain I solved the wrong problem, but the answer seems to work.

--- day09/pt1.py
+++ day09/pt1.py
@@ -5,6 +5,22 @@ with open("input.txt") as f:
     content = [x.strip() for x in f.readlines()]
     # content = [int(x) for x in f.readlines()]
 
-for y, v1 in enumerate(content):
-    for x, v2 in enumerate(v1):
-        pass
+tiles = []
+for v1 in content:
+    x, y =[int(v) for v in v1.split(",")]
+    tiles.append((x,y))
+
+def area (a,b):
+    #print((abs(a[0] - b[0]) + 1), (abs(a[1] - b[1]) + 1))
+    return (abs(a[0] - b[0]) + 1) * (abs(a[1] - b[1]) + 1)
+
+largest = [0, (), ()]
+while tiles:
+    start = tiles.pop()
+    for t in tiles:
+        a = area(start, t)
+        #print([a, start, t])
+        if a > largest[0]:
+            largest = [a, start, t]
+
+print(largest)
