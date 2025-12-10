78d3ebb1b06b99e29ac5fbc06555778017a99e7f
kehvarl <kehvarl@kehvarl.com>
2025-12-09 23:37:37 -0700

    Day 10 - 41:51 / DNF
    First puzzle this year that I can't solve part 2 even with  looking at answers.  It's clearly something mathy and complicated

--- day10/pt2.py
+++ day10/pt2.py
@@ -1,12 +1,29 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 from pprint import pprint
 
-with open("input.txt") as f:
-    content = f.readlines()
-    #content = [int(x) for x in f.readlines()]
+with open("test.txt") as f:
+    content = [x.strip() for x in f.readlines()]
+    # content = [int(x) for x in f.readlines()]
+
+machines = [line.split() for line in content]
+better_machines = []
+
+for m in machines:
+    lights = 0
+    for i,l in enumerate(reversed(m[0][1:-1])):
+        if l == "#":
+            lights |= (1 << int(i))
+    jolts = [int(i) for i in m[-1][1:-1].split(",")]
+    buttons = []
+    for b in m[1:-1]:
+        button = [0 for _ in range(len(m[0][1:-1]))]
+        for i in b[1:-1].split(','):
+            button[int(i)] = 1
+        buttons.append(button)
+
+    better_machines.append([lights, buttons, jolts])
 
 
-for v1 in content:
-    for v2 in content:
-        if int(v1) + int(v2) == 2020:
-            print(v1, v2, (int(v1)*int(v2)))
+for m in better_machines:
+    print(solve_linear(m[2], m[1], None, float("inf"), 0, [0 for _ in range(len(m[2]))], 0, []))
+
